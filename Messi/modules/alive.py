212e29155e06f0308d5a9d77645704fbcea4b5f7
borusara <125817831+borusara@users.noreply.github.com>
2023-06-16 18:43:11 +0530

    Update alive.py

--- Messi/modules/alive.py
+++ Messi/modules/alive.py
@@ -9,24 +9,35 @@ from Messi.events import register
 from Messi import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/c6df4d4c432a2f178bc2f.mp4"
+PHOTO = "https://telegra.ph/file/bda446745e95486a0434e.mp4"
 
-@register(pattern=("awake shoushuke"))
-@register(pattern=("Awake shoushuke"))
-@register(pattern=("Awake Komi"))
+@register(pattern=("Awake Muichiro"))
+@register(pattern=("Awake muichiro"))
+@register(pattern=("Awake muichirou"))
+@register(pattern=("Awake Muichirou"))
+@register(pattern=("Awake tokito"))
+@register(pattern=("Awake Tokito"))
+@register(pattern=("awake Muichiro"))
+@register(pattern=("awake muichiro"))
+@register(pattern=("awake muichirou"))
+@register(pattern=("awake Muichirou"))
+@register(pattern=("awake tokito"))
+@register(pattern=("awake Tokito"))
 @register(pattern=("/awake"))
+@register(pattern=("/Awake"))
 @register(pattern=("/alive"))
+@register(pattern=("/Alive"))
           
           
 
 
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Shoushuke Komi** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Muichiro Tokito** \n\n"
   TEXT += "🗿 **I'm Working Properly** \n\n"
-  TEXT += f"🗿 **My Developers : [Shoushuke creators](https://t.me/shoushuke_updates/3)** \n\n"
+  TEXT += f"🗿 **My Developers : [Muichiro Creators](https://t.me/shoushuke_updates/3)** \n\n"
   TEXT += f"🗿 **Library Version :** `{x}` \n\n"
   TEXT += f"🗿 **Telethon Version :** `{y}` \n\n"
   TEXT += f"🗿 **Pyrogram Version :** `{z}` \n\n"
   TEXT += "**🗿 Thanks For Adding Me Here **"
-  BUTTON = [[Button.url("Help", "https://t.me/shoushuke_komi_bot?start=help"), Button.url("My Headquarters", "https://t.me/shoushuke_support")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Tokito_Muichiro_robot?start=help"), Button.url("My Headquarters", "https://t.me/shoushuke_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
