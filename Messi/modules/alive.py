bd6e530a147cfaf041f84145a4a5be0c9f8d419f
borusara <125817831+borusara@users.noreply.github.com>
2023-06-24 08:30:14 +0530

    Update alive.py

--- Messi/modules/alive.py
+++ Messi/modules/alive.py
@@ -9,7 +9,7 @@ from Messi.events import register
 from Messi import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/bda446745e95486a0434e.mp4"
+PHOTO = "https://telegra.ph/file/56479f8c801e0fe022369.mp4"
 
 @register(pattern=("Awake Muichiro"))
 @register(pattern=("Awake muichiro"))
@@ -32,12 +32,12 @@ PHOTO = "https://telegra.ph/file/bda446745e95486a0434e.mp4"
 
 
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Muichiro Tokito** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kawaki Uzumaki** \n\n"
   TEXT += "🗿 **I'm Working Properly** \n\n"
-  TEXT += f"🗿 **My Developers : [Muichiro Creators](https://t.me/shoushuke_updates/3)** \n\n"
+  TEXT += f"🗿 **My Developers : [Kawaki creators](https://t.me/shoushuke_updates/3)** \n\n"
   TEXT += f"🗿 **Library Version :** `{x}` \n\n"
   TEXT += f"🗿 **Telethon Version :** `{y}` \n\n"
   TEXT += f"🗿 **Pyrogram Version :** `{z}` \n\n"
   TEXT += "**🗿 Thanks For Adding Me Here **"
-  BUTTON = [[Button.url("Help", "https://t.me/Tokito_Muichiro_robot?start=help"), Button.url("My Headquarters", "https://t.me/shoushuke_support")]]
+  BUTTON = [[Button.url("Help", "http://t.me/Uzumaki_Kawaki_Bot?start=help"), Button.url("My Headquarters", "https://t.me/shoushuke_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
